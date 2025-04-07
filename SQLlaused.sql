33dd8339dc5156ea2ea15175d781ddfcd8c2eccb
makspol228 <syas3315@gmail.com>
2025-04-07 11:04:13 +0300

    tabeli loomine sql laused

--- SQLlaused.sql
+++ SQLlaused.sql
@@ -1,1 +1,27 @@
+--sql server managment stuudio
+--(localdb)\MSSQLLocalDB
+--Connect TO:
+--Server Name: (localdb)\MSSQLLocalDB
+--authentification 2 tüüpi:
+--1. windows auth - localdb admini õigused
+--2. sql server auth - kontrollida varem tehtud kasutajad
 
+
+CREATE DATABASE polyakovLOGITpv23;
+USE polyakovLOGITpv23;
+
+--tabel loomine
+--identity(1,1) - ise täidab tabeli 1,2,3...
+--identity(1,1) only for primary key
+CREATE TABLE inimene(
+inimeneID int Primary Key identity(1,1),
+nimi varchar(50) unique,
+synniaeg date,
+telefon char(12),
+pikkus decimal(5,2), --float
+opilaskodu bit
+);
+SELECT * FROM inimene;
+
+--tabeli kustutamine
+DROP table inimene;
